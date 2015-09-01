7232f198ef064156acf0d6c620326c92c47fcfcf
Vinodh <Vinnu@VINODHs-MBP.attlocal.net>
2015-08-31 20:26:18 -0700

    Initial Commit

---
+++ tipcal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcal
+//
+//  Created by VINODH PEDDI on 8/31/15.
+//  Copyright (c) 2015 Peddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
